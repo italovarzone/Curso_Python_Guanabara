80d50f6bc05301d091f43178984b5852e807ed45
italovarzone <84163062+italovarzone@users.noreply.github.com>
2021-08-09 09:43:05 -0300

    Add files via upload

---
+++ ex022.py
@@ -0,0 +1,14 @@
+nome = input('Digite seu nome: ')
+nome = nome.strip()
+print('-' * 28)
+print(nome.upper())
+print(nome.lower())
+print("Seu nome completo tem: {} Caracters".format(len(nome)))
+dividido = nome.split()
+print("Seu primeiro nome tem: {} Letras".format(len(dividido[0])))
+print('-' * 28)
+
+
+
+
+
